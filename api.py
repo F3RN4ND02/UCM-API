52e86198f79f2b0563b7796785dd7d4cba665f15
F3NRN4ND02 <fernando.ujap@gmail.com>
2021-09-16 17:28:38 -0400

    an_advance

--- api.py
+++ api.py
@@ -12,96 +12,117 @@ headers = {
     'Connection': 'close',
 }
 
-data = '{"request": {"action":"challenge","user":"cdrapi","version":"1.0"}}'
-
-# First send the challenge to the UCM.
-
-response = requests.post(url, headers=headers, data=data, verify=False)
-
-# Get the challenge into variable.
-
-a = response.json()
-b = a['response']
-c = b['challenge']
-
-print("This is the challenge number: " + c)
-
-# Getting the challenge with the UCM Api Password with MD5
-# e = input("cdrapi123")
-e = "cdrapi123" # Delete this
-user = c+e
-h = hashlib.md5(user.encode())
-md = h.hexdigest()
-print("This is the Token: " + md)
-
-datas = '{"request":{"action":"login", "token":"' + str(md) + '", "url":"' + str(url) + '" , "user": "cdrapi"}}'
-
-#Send the token to get the Cookie
-
-response2 = requests.post(url, headers=headers, data=datas, verify=False)
-
-#Getting the cookie into a variable
-
-f = response2.json()
-g = f['response']
-cookie = g['cookie']
-
-print("This is the Cookie: " +cookie)
-
-def start():
-    print("Hi, Welcome to the Grandstream Networks UCM - API interaction\n Made by Fernando")
-while True:
-        val = (input("What would you like to do? \nPress a to go to System Configuration. \nPress b to go to Extensions\n").lower())
-        if val.lower() not in ("a", "b", "c", "d"):
-            print("This is not a valid entry")
-        else:
-            break
-
-if val == "a":
-    print("\*---------------System Configuration---------------*/")
-    print("Please select a valid option")
-    option=input("Select 1 to get system status \nSelect 2 to get system general status \n ")
-    if option.lower() not in ("1", "2"):
-        print("This is not a valid entry for System Status")
-        
-    if option == "1":
-        gss = '{"request":{"action":"getSystemStatus", "cookie":"' + str(cookie) + '"}}'
-        response3 = requests.post(url, headers=headers, data=gss, verify=False)
-        rgss = response3.json()
-        gss = rgss['response']
-        print(gss)
-    if option == "2":
-        gss = '{"request":{"action":"getSystemGeneralStatus", "cookie":"' + str(cookie) + '"}}'
-        response3 = requests.post(url, headers=headers, data=gss, verify=False)
-        rgss = response3.json()
-        gss = rgss['response']
-        print(gss)
-    else:
-        val=input("What would you like to do? \nPress a to go to System Configuration. \n").lower()
-if val == "b":
-    print("\*---------------Extension---------------*/\nPlease select a valid option")
-    option=input("Select 1 to get the UCM extensions \nSelect 2 to get SIP Account\n ")
-    if option.lower() not in ("1", "2"):
-        print("This is not a valid entry for Extensions")
-    if option == "1":
-        ext = '{"request":{"action":"listAccount", "cookie":"' + str(cookie) + '", "item_num": "30","options": "extension,account_type,fullname,status,addr", "page": "1", "sidx": "extension", "sord": "asc"}}'
-        response4 = requests.post(url, headers=headers, data=ext, verify=False)
-        rext = response4.json()
-        pext = rext['response']
-        print(pext)
-    if option == "2":
-        extension = input("Would you like to get the SIP account of what extension? Please input\n if there is not a valid ext this will return an error\n")
-        sipaccount = '{"request":{"action":"getSIPAccount", "cookie":"' + str(cookie) + '", "extension":"' + str(extension) + '" }}'
-        request_sip_account = requests.post(url, headers=headers, data=sipaccount, verify=False)
-        # Formatting to JSON
-        json_sip_account = request_sip_account.json()
-        # Get the response in a variable
-        response_SIP_account = json_sip_account['response']
-        returncodestatus = json_sip_account['status']
-        print(response_SIP_account)
-        print(returncodestatus)
-        print(val)
-else:
-    print("This session has ended")
+def auth(url, headers):
     
-start()
+    data = '{"request": {"action":"challenge","user":"cdrapi","version":"1.0"}}'
+
+    # First send the challenge to the UCM.
+
+    response = requests.post(url, headers=headers, data=data, verify=False)
+
+    # Get the challenge into variable.
+
+    a = response.json()
+    b = a['response']
+    c = b['challenge']
+
+    print("This is the challenge number: " + c)
+
+    # Getting the challenge with the UCM Api Password with MD5
+    # e = input("cdrapi123")
+    e = "cdrapi123" # Delete this
+    user = c+e
+    h = hashlib.md5(user.encode())
+    md = h.hexdigest()
+    print("This is the Token: " + md)
+
+    datas = '{"request":{"action":"login", "token":"' + str(md) + '", "url":"' + str(url) + '" , "user": "cdrapi"}}'
+
+    #Send the token to get the Cookie
+
+    response2 = requests.post(url, headers=headers, data=datas, verify=False)
+
+    #Getting the cookie into a variable
+
+    f = response2.json()
+    g = f['response']
+    cookie = g['cookie']
+
+    print("This is the Cookie: " +cookie)
+
+    return cookie
+
+cookie = auth(url, headers)
+val = None
+
+def start(url, headers, cookie, val):
+    val = None
+    print("Hi, Welcome to the Grandstream Networks UCM - API interaction\n**Made by FernandoM NA-LATAM Support**")
+    while True:
+            
+            val = (input("What would you like to do? \nPress a to go to System Configuration. \nPress b to go to Extensions\n").lower())
+            if val.lower() not in ("a", "b", "c", "d"):
+                print("This is not a valid entry")
+            else:
+                break
+    return val
+
+val = start(url, headers,cookie, val)
+
+
+while True:
+    if val == "a":
+        print("\*---------------System Configuration---------------*/")
+        print("Please select a valid option")
+        option=input("Select 1 to get system status \nSelect 2 to get system general status \n ")
+        if option.lower() not in ("1", "2"):
+            print("This is not a valid entry for System Status")
+            
+        if option == "1":
+            gss = '{"request":{"action":"getSystemStatus", "cookie":"' + str(cookie) + '"}}'
+            response3 = requests.post(url, headers=headers, data=gss, verify=False)
+            rgss = response3.json()
+            gss = rgss['response']
+            print(gss)
+        if option == "2":
+            gss = '{"request":{"action":"getSystemGeneralStatus", "cookie":"' + str(cookie) + '"}}'
+            response3 = requests.post(url, headers=headers, data=gss, verify=False)
+            rgss = response3.json()
+            gss = rgss['response']
+            print(gss)
+        else:
+            start(url, headers, cookie, val)
+    if val == "b":
+        print("\*---------------Extension---------------*/\nPlease select a valid option")
+        option=input("Select 1 to get the UCM extensions \nSelect 2 to get SIP Account\n ")
+        if option.lower() not in ("1", "2"):
+            print("This is not a valid entry for Extensions")
+        if option == "1":
+            ext = '{"request":{"action":"listAccount", "cookie":"' + str(cookie) + '", "item_num": "30","options": "extension,account_type,fullname,status,addr", "page": "1", "sidx": "extension", "sord": "asc"}}'
+            response4 = requests.post(url, headers=headers, data=ext, verify=False)
+            rext = response4.json()
+            pext = rext['response']
+            codestatus = rext['status']
+            print(codestatus)
+            print(pext)
+        if option == "2":
+            extension = input("Would you like to get the SIP account of what extension? Please input\n if there is not a valid ext this will return an error\n")
+            sipaccount = '{"request":{"action":"getSIPAccount", "cookie":"' + str(cookie) + '", "extension":"' + str(extension) + '" }}'
+            request_sip_account = requests.post(url, headers=headers, data=sipaccount, verify=False)
+            # Formatting to JSON
+            json_sip_account = request_sip_account.json()
+            # Get the response in a variable
+            response_SIP_account = json_sip_account['response']
+            # Get the status code into a variable
+            codestatus = json_sip_account['status']
+            # Print the reponse
+            print(response_SIP_account)
+            # Print the status code
+            print(codestatus)
+            
+            print(val)
+    else:
+        print("Would you like to make another request?")
+
+        
+    start(url, headers, cookie, val)
---
+++ apiworkbook.py
@@ -0,0 +1,21 @@
+
+
+def function1():
+    var1 = input("writesomething1")
+    var2 = input("write something 2")
+    var3 = input("write something 3")
+    return var1, var2, var3
+
+def function2(var1, var2, var3):
+    print(var1 + var2 + var3)
+    
+var1, var2, var3 = function1()
+function2(var1, var2, var3)
+
+
+function1()
+
+def function3(var1, var2, var3):
+    print(var1 + var2 + var3)
+   
+function3(var1, var2, var3)
